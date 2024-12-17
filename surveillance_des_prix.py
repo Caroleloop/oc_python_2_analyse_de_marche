ec33fbf140e4f994a74092dc7864adb067719bf1
Carole Roch <1rochcarole@gmail.com>
2024-12-17 15:09:53 +0100

    récupération des informations des livres pour une catégorie

--- surveillance_des_prix.py
+++ surveillance_des_prix.py
@@ -2,25 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+ 
 
 
-# ouverture en écriture du fichier
-with open('extrait_informations.csv', 'w', newline='',encoding="utf-8") as fichier:
-    ecrire = csv.writer(fichier)
 
-    #ecriture des colonnes
-    ecrire.writerow(['product_page_url','universal_ product_code (upc)','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url'])
-
-
-    # choix du livre
-    url = 'http://books.toscrape.com/catalogue/set-me-free_988/index.html'
-
+#recuperation des informations du livre
+def scrap_one_book(url, doc):
     reponse = requests.get(url)
-
-    #recuperation des informations du livre
     if reponse.ok:
         soup = BeautifulSoup(reponse.text, "html.parser")
-
         product_page_url = url
         universal_product_code = soup.find('table', class_ ='table table-striped' ).find(string="UPC").find_parent('tr').find('td').text.strip()
         title = soup.find('li',class_ ='active' ).text
@@ -32,12 +22,42 @@ with open('extrait_informations.csv', 'w', newline='',encoding="utf-8") as fichi
         review_rating = soup.find('table', class_ ='table table-striped' ).find(string="Number of reviews").find_parent('tr').find('td').text.strip()
         image_url = soup.find('img').get('src')
         print (product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url)
-
-
-
     #ecrire les informations dans le ficheri csv
-    ecrire.writerow([product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url])
+    doc.writerow([product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url])
 
 
+#lien vers les livres d'une catégorie
+def link_book(nb_next,url_cat):   
+    for i in range (nb_next) : 
+        url = url_cat+str(i)+".html"
+        reponse = requests.get(url)  
+        if reponse.ok:            
+            soup = BeautifulSoup(reponse.text, "html.parser") 
+            # récupérer url d'une page d'une catégorie
+            livres = soup.find_all("h3")
+            for livre in livres :
+                link = livre.find("a")["href"]
+                link_ok = BeautifulSoup(link, "html.parser").text.strip('../../../')
+                lien_livre.append(link_ok)
+    return lien_livre
+    
 
+
+if __name__ == "__main__" :
+    #list url
+    lien_livre = []
+    url_categorie = "https://books.toscrape.com/catalogue/category/books/young-adult_21/page-"
+    link_book(4,url_categorie)
+    print(lien_livre)
+
+
+    # ouverture en écriture du fichier
+    with open('extrait_informations.csv', 'w', newline='',encoding="utf-8") as fichier:
+        ecrire = csv.writer(fichier)
+        #ecriture des colonnes
+        ecrire.writerow(['product_page_url','universal_ product_code (upc)','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url'])
         
+        for i in lien_livre : 
+            url = "https://books.toscrape.com/catalogue/"+str(i)
+            scrap_one_book(url,ecrire)
+
