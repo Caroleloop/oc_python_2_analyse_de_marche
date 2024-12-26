1fced4b2afcad65d05220f041e03104a8225a697
Carole Roch <1rochcarole@gmail.com>
2024-12-26 17:19:19 +0100

    creation enregistrement immage dans dossier img

--- scrap_category.py
+++ scrap_category.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from scrap_book import scrap_one_book
 import csv
-from pathlib import Path
+import os
 
 #nombre de pages de la catégorie choisi
 def nb_page(url_cat):
@@ -54,18 +54,40 @@ def scrap_category(nom_category, url_categoy):
     books_links = category_books_link(url_categoy)
     category_books_dicts = []
 
-    #ceeation des dossiers
-    file = Path("C:\\Formation\\Projet_02\\repo\\output")
-    file.mkdir(exist_ok=True)
-    file_category = Path(file / nom_category)
-    file_category.mkdir(exist_ok=True)
-    file_save = file_category / f"extrait_informations_{nom_category}.csv"
+    #ceation des dossiers
+    # Chemin principal
+    file = os.path.join("C:\\Formation\\Projet_02\\repo\\output")
+    os.makedirs(file, exist_ok=True)
+    # Sous-répertoire pour la catégorie
+    file_category = os.path.join(file, nom_category)
+    os.makedirs(file_category, exist_ok=True)
+    # Chemin pour enregistrer le fichier CSV
+    file_save = os.path.join(file_category, f"extrait_informations_{nom_category}.csv")
+    # Sous-répertoire pour les images
+    file_img = os.path.join(file_category, "img")
+    os.makedirs(file_img, exist_ok=True)
     
     #récuperation des données
     for book_link in books_links:
         book_url = "https://books.toscrape.com/catalogue/" + book_link
         book_dict = scrap_one_book(book_url)
         category_books_dicts.append(book_dict)
+
+    #enregister des images
+    urls_img = [d["image_url"] for d in category_books_dicts]
+    title_book = [d["title"] for d in category_books_dicts]
+    t = 0
+    for url in urls_img: 
+        url_modifie = url.replace("../../", "")
+        url_img = "https://books.toscrape.com/"+url_modifie
+        image_name = title_book[t]
+        output_path = os.path.join(file_img, image_name)
+        response = requests.get(url_img, stream=True)
+        if response.status_code == 200:  # Vérifier si la requête a réussi
+            with open(output_path, "wb") as file:
+                for chunk in response.iter_content(1024):  # Télécharger par morceaux
+                    file.write(chunk)
+        t = t+1
 
     #enregistrement des données
     with open(file_save, 'w', newline='',encoding="utf-8") as fichier:
